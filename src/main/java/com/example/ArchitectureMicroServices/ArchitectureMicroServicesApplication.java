87f2eb96eaa5d406a6e196809210848e89c56365
SEBASTIAN RUIZ <SEBARUIZ01>
2026-01-06 15:30:44 +0100

    feat: Initialize a new Spring Boot application with Maven wrapper and project structure.

---
+++ src/main/java/com/example/ArchitectureMicroServices/ArchitectureMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArchitectureMicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureMicroServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ArchitectureMicroServices/ArchitectureMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArchitectureMicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
