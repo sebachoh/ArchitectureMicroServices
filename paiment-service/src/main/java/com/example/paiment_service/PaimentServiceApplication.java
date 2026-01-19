f7ada14f462a098e0112e5bb17f0a9748cb9e657
Javier Vargas <javargaspa@unal.edu.co>
2026-01-19 14:50:23 +0100

    Paiment porfin sirviendo :)

---
+++ paiment-service/src/main/java/com/example/paiment_service/PaimentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paiment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaimentServiceApplication.class, args);
+	}
+
+}
