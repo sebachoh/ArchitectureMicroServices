85a19b2a51c6febb1e0535e65d5b227be381f6f2
SEBASTIAN RUIZ <SEBARUIZ01>
2026-01-20 22:23:41 +0100

    Replaced some methods that didnt work

--- catalogue-service/src/main/java/com/example/catalogue/service/ProductService.java
+++ catalogue-service/src/main/java/com/example/catalogue/service/ProductService.java
@@ -53,6 +53,8 @@ public class ProductService {
         productRepository.deleteById(id);
     }
 
+
+
     // Check if product has sufficient stock
     @SuppressWarnings("null")
     public boolean hasStock(Long productId, int requestedQuantity) {
